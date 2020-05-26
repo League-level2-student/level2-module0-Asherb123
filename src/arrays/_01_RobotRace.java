b5397ad51a78e0dc8fa793ee5a6e09cfb3fa965e
Asherb123 <46113623+Asherb123@users.noreply.github.com>
2020-05-25 18:28:19 -0700

    yyeyeyey

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.Random;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
@@ -9,19 +11,36 @@ public static void main(String[] args) {
 
 	//2. create an array of 5 robots.
 	Robot[] robots= new Robot [5];
+	
 	//3. use a for loop to initialize the robots.
 	for (int i = 0; i < robots.length; i++) {
 		robots [i] = new Robot(150*i+100,540);
-		
+		robots [i].setSpeed(10);
 		
 	}
 	//4. make each robot start at the bottom of the screen, side by side, facing up
 		
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
-
+	Random rand = new Random();
+	
+	
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
+	boolean racing = true;
+	while (racing ) {
+		for (int i = 0; i < robots.length; i++) {
+		int speed =rand.nextInt(100);
+		robots [i].move(speed);
+		if (robots [i].getY()<=100) {
+		robots [i].setSpeed(0);
+		racing=false;
+		robots[i].sparkle();
+		
+		}
+		}
+	
+		
+	}
 	//7. declare that robot the winner and throw it a party!
 
 	//8. try different races with different amounts of robots.
